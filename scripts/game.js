a081681e61752c8d2c47696dcd7541df459db587
ZigZiggyZag <nicka.j384@gmail.com>
2021-08-14 17:49:44 -0700

    simple ball bouncing around

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,32 @@
 var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
 
-context.fillStyle = "#FF00FF";
-context.fillRect(20, 40, 50, 50);
+var x = canvas.width/2;
+var y = canvas.height/2;
+
+function getRandomSign()
+{
+    return Math.random() < 0.5 ? -1 : 1;
+}
+
+var dx = 2 * getRandomSign();
+var dy = 2 * getRandomSign();
+
+function draw()
+{
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.beginPath();
+    context.arc(x, y, 10, 0, Math.PI*2);
+    context.fillStyle = "#FF0000";
+    context.fill();
+    context.closePath();
+
+    dx = (x <= 5 || x >= canvas.width - 5) ? -dx : dx;
+    dy = (y <= 5 || y >= canvas.height - 5) ? -dy: dy;
+
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);

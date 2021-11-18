9d709d9fa20882de388586cca90fe43e5374803e
ZigZiggyZag <nicka.j384@gmail.com>
2021-11-17 18:44:00 -0800

    test map image player drawn in center initial code for map traversal

--- scripts/assets.js
+++ scripts/assets.js
@@ -2,4 +2,12 @@ var smileyFace = new Image();
 smileyFace.src = 'assets/SmileyFace.png';
 smileyFace.alt = 'smiley face';
 
-export {smileyFace};
+var player = new Image();
+player.src = 'assets/Player.png';
+player.alt = 'player';
+
+var area1 = new Image();
+area1.src = 'assets/home.png';
+area1.alt = 'area1';
+
+export {smileyFace, player, area1};
--- scripts/game.js
+++ scripts/game.js
@@ -1,50 +1,27 @@
 import {mouseX, mouseY, inCanvas, up, dw, lf, rt} from './input.js';
-import { smileyFace } from './assets.js';
+import { smileyFace, player, area1 } from './assets.js';
 
 var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
 
-var x = canvas.width/2;
-var y = canvas.height/2;
-
-var cX = canvas.width/2;
-var cY = canvas.height/2;
-
-function getRandomSign()
-{
-    return Math.random() < 0.5 ? -1 : 1;
-}
-
-function drawCircle(x, y, radius, color)
-{
-    context.beginPath();
-    context.arc(x, y, radius, 0, Math.PI*2);
-    context.fillStyle = color;
-    context.fill();
-    context.closePath();
-}
+var cX = 640 - canvas.width/2;
+var cY = 640 - canvas.height/2;
 
 function draw(e)
 {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    if(inCanvas)
-    {
-        x = mouseX - (smileyFace.width / 2);
-        y = mouseY - (smileyFace.height / 2);
-    }
+    context.drawImage(area1, 0 + cX, 0 + cY, canvas.width + cX, canvas.height + cY, 0, 0, canvas.width + cX, canvas.height + cY);
 
-    context.drawImage(smileyFace, x, y);
+    context.drawImage(player, 0, 0, 64, 64, canvas.width/2 - 32, canvas.height/2 - 32, 64, 64);
 
-    context.fillText('X: ' + mouseX, 5, 10);
-    context.fillText('Y: ' + mouseY, 5, 20);
+    context.fillText('X: ' + cX, 5, 10);
+    context.fillText('Y: ' + cY, 5, 20);
 
-    drawCircle(cX, cY, 10, "#FF0000")
-
-    if (up) { cY -= 2 };
-    if (dw) { cY += 2 };
-    if (lf) { cX -= 2 };
-    if (rt) { cX += 2 };
+    if (up && (cY > 0)) { cY -= 2 };
+    if (dw && (cY < 1280 - canvas.height)) { cY += 2 };
+    if (lf && (cX > 0)) { cX -= 2 };
+    if (rt && (cX < 1280 - canvas.width)) { cX += 2 };
 }
 
 setInterval(draw, 10);
